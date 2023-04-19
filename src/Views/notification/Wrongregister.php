09bde9a0ff7a31e177f545bed93038de9d3cd0e4
Le Huy <103082623+lgh2003@users.noreply.github.com>
2023-04-19 10:40:49 +0700

    Add files via upload
    Update Wronglogin.php and Wrongregister.php for notification

--- src/Views/notification/Wrongregister.php
+++ src/Views/notification/Wrongregister.php
@@ -1,10 +1,10 @@
 <?php
     class Wrongregister{
         function printError($display,$content){
-            // echo "<h3 style='diplay:$display;position: relative;z-index:100;' class='text-center text-uppercase align-center text-white'>$content</h3>";
-            echo "<div style='opacity:0.1;font-size:initial;diplay:$display;position: fixed;bottom:5px;right:5px;z-index:100;opacity:1;' class='m-0 toast' >
-            <div class='toast-header text-dark'>Thông báo</div>
-            <div class='toast-body bg-primary text-white'>
+            // echo "<h3 style='diplay:$display;position: relative;z-index:100;' class='text-center text-uppercase align-center text-white'>$content</h3>"; 
+            echo "<div style='font-size:initial;diplay:$display;position:fixed;bottom:90%;right:45%;z-index:100;opacity:1;' class='m-0 toast'>
+            <div class='toast-body bg-dark text-center text-white'>Thông báo</div>
+            <div class='toast-body bg-dark text-center text-white'>
                 $content
             </div>
           </div>";
